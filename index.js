386619912faa9af608d85bba06e27c18d1954f58
Nastya07s <nastya-kotovich@mail.ru>
2018-11-07 20:52:13 +0300

    Add src code

---
+++ index.js
@@ -0,0 +1,6 @@
+function go(){
+  var input1 = +document.getElementById('input1').value, 
+  input2 = +document.getElementById('input2').value, 
+  result = document.getElementById('result');
+  result.innerHTML = input1 + input2;
+}
