5d7522ef6be167592deed60ee482fa3171ffcf02
Nastya07s <44784575+Nastya07s@users.noreply.github.com>
2018-11-07 20:57:35 +0300

    Update index.js
    changes in the visibility of variables

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function go(){
-  var input1 = +document.getElementById('input1').value, 
+  let input1 = +document.getElementById('input1').value, 
   input2 = +document.getElementById('input2').value, 
   result = document.getElementById('result');
   result.innerHTML = input1 + input2;
